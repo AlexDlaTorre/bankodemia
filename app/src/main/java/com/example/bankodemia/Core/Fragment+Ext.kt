06f91393aed32f349eb3c81f969bddac5b520939
AlexDlaTorre <94090197+AlexDlaTorre@users.noreply.github.com>
2022-03-03 16:58:40 -0600

    Extension in class fragments
    Extension for the transition from anActivity to a Fragment in file  Fragment+ext.kt

--- app/src/main/java/com/example/bankodemia/Core/Fragment+Ext.kt
+++ app/src/main/java/com/example/bankodemia/Core/Fragment+Ext.kt
@@ -2,10 +2,15 @@ package com.example.bankodemia.Core
 
 import android.content.Context
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.bankodemia.R
+import com.example.bankodemia.UI.View.LoginFragment
 import com.google.android.material.snackbar.Snackbar
 
 fun Context.toast(message: String, duration: Int) {
@@ -26,5 +31,18 @@ fun Fragment.showSnackBarActionMessage(message: String, actionTitle: String, dur
         setAction(actionTitle, actionListener)
         setActionTextColor(ContextCompat.getColor(requireContext(), R.color.white))
         show()
+    }
+}
+
+fun TransitionFragment(button: Button,fragmentName: Fragment,supportFragmentManager: FragmentManager) {
+    val buttonName = button
+    button.setOnClickListener{
+        val fragment = fragmentName
+        val fragmentManager: FragmentManager = (supportFragmentManager)
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction.add(android.R.id.content, fragmentName)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
