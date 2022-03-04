1d3a8802b48b948f2a59bc8ba8a271899e7c134f
Javier Pineda <46772638+jpgpineda@users.noreply.github.com>
2022-03-03 21:21:59 -0600

    adding extension to encode and decode images in base64

---
+++ app/src/main/java/com/example/bankodemia/Core/ImageView+Ext.kt
@@ -0,0 +1,14 @@
+package com.example.bankodemia.Core
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.widget.ImageView
+import java.io.ByteArrayOutputStream
+
+fun ImageView.toBase64(bitMap: Bitmap): String {
+    val baos = ByteArrayOutputStream()
+    bitMap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+    val imageBytes = baos.toByteArray()
+    return Base64.encodeToString(imageBytes, Base64.DEFAULT)
+}
--- app/src/main/java/com/example/bankodemia/Core/String+Ext.kt
+++ app/src/main/java/com/example/bankodemia/Core/String+Ext.kt
@@ -1,5 +1,8 @@
 package com.example.bankodemia.Core
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import java.util.regex.Pattern
 
 fun String.isEmailValid(): Boolean {
@@ -7,6 +10,11 @@ fun String.isEmailValid(): Boolean {
     val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
     val matcher = pattern.matcher(this)
     return matcher.matches()
+}
+
+fun String.base64ToImage(): Bitmap {
+    val imageBytes: ByteArray = Base64.decode(this, Base64.DEFAULT)
+    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
 }
 
 var String.empty: String
