5124d4b4ef29bdb337655ac7ab9be1f4fe1d2f82
Jose Luis Somilleda <joseluissom004@gmail.com>
2022-03-19 12:11:59 -0600

    Correcion de color de fondo, correcion del shared preference para dejar el nombre del applicationId

--- app/src/main/java/com/example/bankodemia/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/bankodemia/ui/view/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.bankodemia.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bankodemia.core.instances.SharedPreferencesInstance
 import com.example.bankodemia.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mSharedPreferences: SharedPreferencesInstance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        mSharedPreferences = SharedPreferencesInstance.getInstance(this)
+
         setContentView(mBinding.root)
     }
 }
