1d868134a669c9bc0d4e867c2365c24b271b9c79
AlexDlaTorre <94090197+AlexDlaTorre@users.noreply.github.com>
2022-03-26 02:03:24 -0600

    Cards fragment function set id as card number

--- app/src/main/java/com/example/bankodemia/ui/view/CardsFragment.kt
+++ app/src/main/java/com/example/bankodemia/ui/view/CardsFragment.kt
@@ -6,56 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.bankodemia.R
 import com.example.bankodemia.core.showSnackBarMessage
 import com.example.bankodemia.core.utils.*
 import com.example.bankodemia.databinding.FragmentCardsBinding
 import com.example.bankodemia.domain.domainObjects.User.geUserProfile.UserProfileDTO
 import com.example.bankodemia.ui.viewModel.CardsViewModel
-import com.example.bankodemia.ui.viewModel.HomeViewModel
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_cards.*
 
 class CardsFragment : Fragment() {
 
     private var _binding: FragmentCardsBinding? = null
     private val binding get() = _binding!!
     private lateinit var bankList:Array<String>
-    private lateinit var viewModel: CardsViewModel
-    private lateinit var communicator: FragmentCommunicator
-
+    private lateinit var cardsViewModel: CardsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val cardsViewModel =
-            ViewModelProvider(this).get(CardsViewModel::class.java)
+        cardsViewModel = ViewModelProvider(this).get(CardsViewModel::class.java)
         bankList = arrayOf("Bankodemia","HSBC","CityBanamex","Bancomer","BBVA")
 
         _binding = FragmentCardsBinding.inflate(inflater, container, false)
-        val randomCvv = RandomNumber(100,199).roll().toString()
-        val randomCardNumber = RandomNumber(1000,1999).roll().toString()
-        val randomMonth = RandomNumber(1,12).roll().toString()
-        val randomDay = RandomNumber(1,30).roll().toString()
 
-        binding.cardsEtCvv.text = randomCvv
-        binding.cardsTvValidityDate.text = "$randomMonth/$randomDay"
-//        binding.cardsTvCardNumber.text = "543924647664$randomCardNumber"
-        viewModel.getUserProfileData()
+        cardsViewModel.getUserProfileData()
         setupObservers()
-
-
-
-        println(RandomString(bankList).rollBank())
-        println(binding.cardsEtCvv.text )
+        setupDateCvv()
 
         return binding.root
     }
 
+
+
     private fun setupObservers() {
-        viewModel.uiStateEmitter.observe(viewLifecycleOwner) { updateUI(it) }
+        cardsViewModel.uiStateEmitter.observe(viewLifecycleOwner) { updateUI(it) }
     }
 
     private fun updateUI(uiState: BaseUiState) {
@@ -72,6 +57,15 @@ class CardsFragment : Fragment() {
         }
     }
 
+    fun setupDateCvv() {
+        val randomCvv = RandomNumber(100,199).roll().toString()
+        val randomMonth = RandomNumber(1,12).roll().toString()
+        val randomDay = RandomNumber(1,30).roll().toString()
+        binding.apply {
+            cardsEtCvv.text = randomCvv
+            cardsTvValidityDate.text = "$randomMonth/$randomDay"
+        }
+    }
 
 
     fun setupCardNumber(user: UserProfileDTO) {
