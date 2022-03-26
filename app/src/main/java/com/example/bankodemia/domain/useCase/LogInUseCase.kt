7c85662e3866a9521e2745851c7d6e3d95696aa0
Jose Luis Somilleda <joseluissom004@gmail.com>
2022-03-25 19:26:46 -0600

    Se ajusta el login con los cambios de los errores

--- app/src/main/java/com/example/bankodemia/domain/useCase/LogInUseCase.kt
+++ app/src/main/java/com/example/bankodemia/domain/useCase/LogInUseCase.kt
@@ -1,11 +1,12 @@
 package com.example.bankodemia.domain.useCase
 
 import com.example.bankodemia.data.model.Auth
+import com.example.bankodemia.data.model.BankodemiaError
 import com.example.bankodemia.data.repository.LogInRepository
 import com.example.bankodemia.domain.domainObjects.Auth.AuthDTO
 
 class LogInUseCase {
     private val repository = LogInRepository()
 
-    suspend operator fun invoke(login: Auth.AuthLogIn): AuthDTO? = repository.logIn(login)
+    suspend operator fun invoke(login: Auth.AuthLogIn): Pair<AuthDTO? , BankodemiaError?> = repository.logIn(login)
 }
